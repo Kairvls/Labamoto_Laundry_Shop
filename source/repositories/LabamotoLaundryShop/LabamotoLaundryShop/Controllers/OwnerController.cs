bcbefe0dbff31afac72d46773f3114b36d7a2c4b
unknown <jhoncarlosuan26@gmail.com>
2025-11-03 02:35:48 +0800

    Updated login UI and dashboard sample

---
+++ source/repositories/LabamotoLaundryShop/LabamotoLaundryShop/Controllers/OwnerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabamotoLaundryShop.Controllers
+{
+    public class OwnerController : Controller
+    {
+        // Owner Dashboard Page
+        public ActionResult Dashboard()
+        {
+            // Check if logged in
+            if (Session["OwnerUsername"] == null)
+                return RedirectToAction("OwnerLogin", "Account");
+
+            ViewBag.OwnerName = Session["OwnerUsername"];
+            return View();
+        }
+    }
+}
